d6463bf6d0f04a9f75556c899e90c8f5090ddccf
dylancastanhinha <castanhinha1@gmail.com>
2017-04-04 18:48:37 -0400

    Added test methods for desktop

--- WeightManagement/src/main/java/FragmentControllers/CurrentClientsOrTrainerFragment.java
+++ WeightManagement/src/main/java/FragmentControllers/CurrentClientsOrTrainerFragment.java
@@ -120,6 +120,9 @@ public class CurrentClientsOrTrainerFragment extends Fragment {
         public void testMethod(){
 
             Log.i("AppInfo", "Again a test method");
+            Log.i("AppInfo", "Test to see if i can pull from my desktop");
+            Log.i("AppInfo", "Again a test method");
+            Log.i("AppInfo", "Test to see if i can pull from my desktop");
 
         }
 
